bf0934f5d3ae0e72eafda2b2c66b34a2357b0865
BilelMatallah <bilelmatallah@hotmail.com>
2023-03-11 13:19:57 +0000

    feat: opción de borrado añadida

--- src/js/component/Formulario.jsx
+++ src/js/component/Formulario.jsx
@@ -1,35 +1,52 @@
-import propTypes from "prop-types";
-//import React, { useState } from "react";
+import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
-const Formulario = (props) => {
 
-    const [task, setTask] = useState ("");
+const Formulario = () => {
+
+    const [inputValue, setInputValue] = useState ("");
+
+    //Lista de Tareas
+    const [tareas, setTareas] = useState ([])
     
+    //onChange
     const handleChange = (e) => {
-        setTask(e.target.value);
-        console.log(task)
+        setInputValue(e.target.value);
     };
 
-    const handleDown = (e) => {
-        if (e.key == "ENTER"){
-            props.addNewTask(task)
-        };
-    };
+    //Guardar nueva tarea
+    const añadirTarea = (e) => {
+        if(e.key === "Enter") {
+            setTareas(tareas.concat(inputValue));
+            setInputValue("")}
+        }
 
     return (
         <div>
             <h3>Mis tareas</h3>
-            <input onChange={handleChange} type="text" name="tarea" onKeyDown = {handleDown} value = {task}/>
+            <input onChange={handleChange} type="text" name="tarea" onKeyDown = {añadirTarea} value = {inputValue}/>
             
             <div class="card">
                 <div class="card-body">
-                    <p class="card-text"> {task} </p>
+                {tareas.map((nuevaTarea, index) => (
+                        //Aquí se añade la nueva tarea y el icono <FaTrash /> que al hacer click en él, borramos la tarea
+                        <li class="card-text"> {nuevaTarea}
+                            <FaTrash onClick={() => 
+                                setTareas (
+                                    tareas.filter (
+                                        (tareas, currentIndex) =>
+                                        index != currentIndex
+                                    )
+                                )
+                            }/>
+                        </li>))
+                }  
                 </div>
             </div>
 
         </div>
     );
+}
 
-};
 
-//export default Formulario ;
+export default Formulario ;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,39 +1,14 @@
 import React, {useState} from "react";
-//import Formulario from "./Formulario.jsx";
+import Formulario from "./Formulario.jsx";
 
 
 
 //create your first component
 const Home = () => {
 
-const [inputValue, setInputValue] = useState ("");
-
-const [tareas, setTareas] = useState ([])
-
-const añadirTarea = (e) => {
-    if(e.key === "Enter") {
-        setTareas(tareas.concat(inputValue));
-        setInputValue("");
-    }
-
-};
-
 	return (
 		<div className="container">
-			<ul>
-                <h1>Todos</h1>
-                <li>
-                    <input type="text" onChange={(e) =>setInputValue(e.target.value)} 
-                    onKeyDown={añadirTarea} placeholder="What do you have to do?" value={inputValue}/>
-                </li>
-                <li>
-                    {tareas.map((nuevaTrea) => (
-                        <li>
-                            {nuevaTrea}
-                        </li>
-                    ))}
-                </li>
-            </ul>
+			<Formulario />
 		</div>
 	);
 };
